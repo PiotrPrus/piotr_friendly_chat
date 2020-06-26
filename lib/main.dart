538eb4120f09a0ccc60ec499a04ddd590189bb49
piotrprus <piotr.prus@schibsted.com>
2020-06-26 15:49:12 +0200

    add platform specific themesū

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,17 @@ void main() {
 
 String _name = "Piotr";
 
+final ThemeData kIOSTheme = ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
+
 class FriendlyChatApp extends StatelessWidget {
   const FriendlyChatApp({
     Key key,
